678aa1155fe0b761329059e9bbc9e5dbcc36825f
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2025-07-14 12:19:05 +0700

    feat: implement login functionality with validation; add LoginDto and login schema; update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,52 @@
+import { login } from "@/api/users/userAuth";
 import { LoginForm } from "@/components/LoginForm";
-import Image from "next/image";
-import React from "react";
+import {
+  loginFormSchema,
+  type loginFormData,
+} from "@/models/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { startTransition, useActionState } from "react";
+import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginFormData>({
+    resolver: zodResolver(loginFormSchema),
+  });
+  const router = useRouter();
+
+  const [state, submitAction, isPending] = useActionState(
+    async (prevState: any, formData: loginFormData) => {
+      try {
+        const token = await login(formData);
+        if (token) {
+          Cookies.set("authToken", token, {
+            expires: 1,
+            path: "/",
+            secure: true,
+            sameSite: "Strict",
+          });
+
+          router.push("/dashboard");
+        }
+      } catch (error: any) {
+        console.log("Login Error");
+      }
+    },
+    undefined
+  );
+
+  const onSubmit = (data: loginFormData) => {
+    startTransition(() => {
+      submitAction(data);
+    });
+  };
+
   return (
     <div className="flex flex-1 min-h-screen items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
