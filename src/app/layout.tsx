f4138645d8c54586242b0d0dda91820e5ad88b55
Minh Quan <doquan19062004@gmail.com>
2025-08-09 01:12:59 +0700

    feat: restructure uploadthing API by moving core and route files to app/api directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import './globals.css';
 import { AppProvider } from '@/contexts/AppContext';
 import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin';
 import { extractRouterConfig } from 'uploadthing/server';
-import { ourFileRouter } from '@/api/uploadthing/core';
+import { ourFileRouter } from '@/app/api/uploadthing/core';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
