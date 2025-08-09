7aa070b98b76a54c67e1cd355e05cb746aee1400
Minh Quan <doquan19062004@gmail.com>
2025-08-09 09:39:59 +0700

    feat: format updatedAt date in employee columns and clean up code

--- src/app/dashboard/employees/columns.tsx
+++ src/app/dashboard/employees/columns.tsx
@@ -13,6 +13,7 @@ import { Type, TYPE_LABELS } from '@/enums/typeEnum';
 import { EmployeeDto } from '@/models/dto/employeeDto';
 import { ColumnDef } from '@tanstack/react-table';
 import { Edit, MoreHorizontal, Trash } from 'lucide-react';
+import { format } from 'date-fns';
 
 export const columns: ColumnDef<EmployeeDto>[] = [
   {
@@ -45,15 +46,19 @@ export const columns: ColumnDef<EmployeeDto>[] = [
     cell: ({ row }) => {
       const employee = row.original.type as Type;
       return <div>{TYPE_LABELS[employee]}</div>;
-    }
+    },
   },
-  {
-    accessorKey: 'departmentId',
-    header: 'PHÒNG BAN',
-  },
+  // {
+  //   accessorKey: 'departmentId',
+  //   header: 'PHÒNG BAN',
+  // },
   {
     accessorKey: 'updatedAt',
-    header: 'NGÀY CẬP NHẬT',
+    header: () => <div className="text-center">CẬP NHẬT LÚC</div>,
+    cell: ({ row }) => {
+      const updatedAt = row.original.updatedAt;
+      return <div className="text-center">{updatedAt ? format(new Date(updatedAt), 'dd/MM/yyyy HH:mm:ss') : ''}</div>;
+    },
   },
   {
     accessorKey: 'actions',
@@ -70,11 +75,11 @@ export const columns: ColumnDef<EmployeeDto>[] = [
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => (resource)}>
+              <DropdownMenuItem onClick={() => resource}>
                 <Edit className="w-4 h-4 mr-2" />
                 Chỉnh sửa
               </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => (resource)} className="text-red-500 focus:text-red-500">
+              <DropdownMenuItem onClick={() => resource} className="text-red-500 focus:text-red-500">
                 <Trash className="w-4 h-4 mr-2" />
                 Xoá
               </DropdownMenuItem>
