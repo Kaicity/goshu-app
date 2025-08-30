3c08960cde3574fff337ee07beea680ca71dfd7e
Minh Quan <doquan19062004@gmail.com>
2025-08-31 00:54:52 +0700

    feat: enhance LeaveRequestDialog with date selection and input fields

--- src/app/dashboard/employees/leave-request/LeaveRequestDialog.tsx
+++ src/app/dashboard/employees/leave-request/LeaveRequestDialog.tsx
@@ -1,21 +1,48 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { DialogDescription } from '@radix-ui/react-dialog';
+import { Label } from '@/components/ui/label';
+import { DatePicker } from '@/components/date-picker';
+import { Input } from '@/components/ui/input';
 
 interface LeaveRequestDialogProps {
-    open: boolean;
-    setOpen: (open: boolean) => void;
+  open: boolean;
+  setOpen: (open: boolean) => void;
 }
 
 export function LeaveRequestDialog({ open, setOpen }: LeaveRequestDialogProps) {
+  const [startDateSelected, setStartDateSelected] = useState<Date>(new Date());
+  const [endDateSelected, setEndDateSelected] = useState<Date>(new Date());
+  const handleStartDateChange = (date: Date | undefined) => {
+    if (!date) return;
+    setStartDateSelected(date);
+  };
+  const handleEndDateChange = (date: Date | undefined) => {
+    if (!date) return;
+    setEndDateSelected(date);
+  };
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
-            <DialogTitle>Đơn xin nghỉ phép</DialogTitle>
-            <DialogDescription>Điền thông tin nghỉ phép</DialogDescription>
+          <DialogTitle>Đơn xin nghỉ phép</DialogTitle>
+          <DialogDescription>Điền thông tin nghỉ phép</DialogDescription>
         </DialogHeader>
+        <div className="grid grid-cols-2 space-x-2">
+          <div className="flex flex-col col-span-3 md:col-span-1 gap-2">
+            <Label>Ngày bắt đầu</Label>
+            <DatePicker onDateChange={handleStartDateChange} dateValue={startDateSelected} />
+          </div>
+          <div className="flex flex-col col-span-3 md:col-span-1 gap-2">
+            <Label>Ngày kết thúc</Label>
+            <DatePicker onDateChange={handleEndDateChange} dateValue={endDateSelected} />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 col-span-3 md:col-span-1">
+          <Label>Lí do</Label>
+          <Input placeholder="Lí do " className="h-12"  />
+        </div>
       </DialogContent>
     </Dialog>
   );
